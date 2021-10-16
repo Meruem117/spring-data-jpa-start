d2e037397396a883639d0db878ef509514adbb35
Meruem117 <1572677319@qq.com>
2021-10-16 23:32:59 +0800

    update log

---
+++ src/main/java/niit/start/entity/Log.java
@@ -0,0 +1,37 @@
+package niit.start.entity;
+
+import javax.persistence.Entity;
+import java.io.Serializable;
+
+/**
+ * @auther: Meruem117
+ */
+@Entity
+public class Log implements Serializable {
+    private static final long serialVersionUID = 3569853174776315537L;
+
+    private String author;
+    private String bvid;
+    private String location;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getBvid() {
+        return bvid;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    @Override
+    public String toString() {
+        return "Log{" +
+                "author='" + author + '\'' +
+                ", bvid='" + bvid + '\'' +
+                ", location='" + location + '\'' +
+                '}';
+    }
+}
--- src/main/java/niit/start/util/LogGenerator.java
+++ src/main/java/niit/start/util/LogGenerator.java
@@ -1,12 +1,10 @@
 package niit.start.util;
 
 import lombok.extern.slf4j.Slf4j;
+import niit.start.entity.Log;
 import niit.start.entity.VList;
 import niit.start.repository.VListRepository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -19,16 +17,16 @@ public class LogGenerator {
     @Resource
     private VListRepository VListRepository;
 
-    @RequestMapping("/generate")
+    @GetMapping("/generate")
     @ResponseBody
-    public boolean generateLog(int n) {
+    public boolean generateLog(@RequestParam int number) {
         List<VList> list;
         list = VListRepository.findAll();
         String[] city = {"上海", "广东", "北京", "江苏", "浙江"};
         Random random = new Random();
         boolean res = false;
         try {
-            for (int i = 0; i < n; i++) {
+            for (int i = 0; i < number; i++) {
                 int t1 = random.nextInt(list.size());
                 int t2 = random.nextInt(city.length);
                 log.info("{}:{} from {}", list.get(t1).getAuthor(), list.get(t1).getBvid(), city[t2]);
@@ -42,8 +40,10 @@ public class LogGenerator {
 
     @PostMapping("/add")
     @ResponseBody
-    public void addLog(String author, String bvid, String location) {
-        String loc = location.split(" ")[0];
+    public void addLog(@RequestBody Log logItem) {
+        String author = logItem.getAuthor();
+        String bvid = logItem.getBvid();
+        String loc = logItem.getLocation().split(" ")[0];
         log.info("{}:{} from {}", author, bvid, loc);
     }
 }
